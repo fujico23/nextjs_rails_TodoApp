05cd0d0828e680bf5a1b557685b0da7bf971d256
fujico23 <k03c347b@yahoo.co.jp>
2025-01-31 21:46:33 +0900

    feat: Todo編集機能を追加し、データ取得と更新のカスタムフックを実装

--- front/src/app/lib/action.ts
+++ front/src/app/lib/action.ts
@@ -1,7 +1,7 @@
 "use client";
 import { useCallback, useEffect, useState } from 'react';
 import { Todo } from "@/interfaces/index";
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 
 // Todo一覧を取得するカスタムフック
 export const useFetchTodos = () => {
@@ -90,4 +90,68 @@ export const useCreateTodo = () => {
   };
 
   return { title, setTitle, content, setContent, loading, error, handleSubmit };
+}
+
+// Todoを更新するページのデータを取得するカスタムフック
+export const useFetchEditTodo = () => {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<Error | null>(null);
+  const currentPath = usePathname();
+  const id = currentPath.split("/")[2];
+
+  const fetchEditTodo = useCallback(async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/todos/${id}`);
+      if (!res.ok) {
+        throw new Error('エラーが発生しました');
+      }
+      const json = await res.json();
+      setTitle(json.title);
+      setContent(json.content);
+    } catch (error) {
+      console.error(error);
+      setError(error instanceof Error ? error : new Error(String(error)));
+    }
+    setLoading(false);
+  }, [id]);
+
+  useEffect(() => {
+    fetchEditTodo();
+  }, [fetchEditTodo]);
+
+  return { title, setTitle, content, setContent, loading, error };
+}
+
+// Todoを更新するカスタムフック
+export const usePutTodo = () => {
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [error, setError] = useState<Error | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const currentPath = usePathname();
+  const id = currentPath.split("/")[2];
+  const router = useRouter();
+
+  const handleEditForm = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await fetch(`http://localhost:3000/todos/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          title: title,
+          content: content,
+        }),
+      });
+      router.push(`/todos`);
+    } catch (error) {
+      console.error(error);
+      setError(error instanceof Error ? error : new Error(String(error)));
+    }
+  }
+  return { title, setTitle, content, setContent, loading, setLoading, error, setError,handleEditForm };
 }
