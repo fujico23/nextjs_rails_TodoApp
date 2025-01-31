00ec788ce71a82aadc3032a1ab3f727fadd78941
fujico23 <k03c347b@yahoo.co.jp>
2025-01-31 20:41:07 +0900

    feat: Todo詳細ページのスタイルを改善し、TOP一覧リンクを追加

--- front/src/app/todos/[id]/page.tsx
+++ front/src/app/todos/[id]/page.tsx
@@ -10,10 +10,22 @@ export default function Page() {
   if (!todo) return <div>データがありません</div>;
 
   return (
-    <div>
-      {todo.title}
-      {todo.content}
-      <Link href={`/todos/${id}/edit`}>編集</Link>
+    <div className="flex items-center justify-center flex-col w-96 
+    h-96 mx-auto my-auto p-4 bg-white shadow-lg rounded-lg">
+      <h1 className="text-3xl pb-2">{todo.title}</h1>
+      <p className="text-xl">{todo.content}</p>
+      <Link
+        href={`/todos/${id}/edit`}
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+      >
+        編集
+      </Link>
+      <Link
+        href="/todos"
+        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+      >
+        TOP一覧
+      </Link>
     </div>
   );
 }
